8efb568b36d23fb695b5b0452e5fba2424712b30
kevintownsend <kt@krtownsend.com>
2015-10-06 16:21:56 -0500

    .

--- makefile
+++ makefile
@@ -1,2 +1,14 @@
 vim :
 	vim -p makefile multipumped_memory.v
+
+tcl: common.vh
+	vivado -mode tcl < compile.tcl
+
+common.vh: abs.vh log2.vh
+	cp ../common/common.vh .
+
+abs.vh:
+	cp ../common/abs.vh .
+
+log2.vh:
+	cp ../common/log2.vh .
--- multipumped_memory.v
+++ multipumped_memory.v
@@ -1,37 +1,36 @@
-module multipumped_memory
-#(
+module multipumped_memory(clk, wr, addr, d, q);
     parameter PORTS = 8;
     parameter WIDTH = 64;
     parameter DEPTH = 512;
     parameter LOG2_DEPTH = log2(DEPTH - 1);
     parameter LOG2_PORTS = log2(PORTS - 1);
-)(
+    input clk;
     input [0:PORTS - 1] wr;
     input [PORTS * LOG2_DEPTH - 1:0] addr;
     input [PORTS * WIDTH - 1:0] d;
-    output [PORTS * WIDTH - 1:0] q;
-)
-reg [LOG2_PORTS - 1:0] counter;
+    output reg [PORTS * WIDTH - 1:0] q;
+reg [LOG2_PORTS - 2:0] counter;
 initial counter = 0;
 always @(posedge clk) begin
     counter <= counter + 1;
-    if(counter[LOG2_PORTS - 1])
-        counter[LOG2_PORTS - 1] <= 0;
 end
-reg [LOG2_PORTS - 2:0] counter_pipe;
-always @(posedge clk) begin
-    counter_pipe <= counter;
-end
-
-integer i;
+integer i, j;
 reg [LOG2_DEPTH - 1:0] addr_2d [0:PORTS - 1];
 reg [WIDTH - 1:0] d_2d [0:PORTS - 1];
 always @* for(i = 0; i < PORTS; i = i + 1) begin
-    addr_2d[i] = addr[(i + 1) * LOG2_DEPTH - 1:LOG2_DEPTH];
+    addr_2d[i] = addr[(i + 1) * LOG2_DEPTH - 1 -:LOG2_DEPTH];
     d_2d[i] = d[(i + 1) * WIDTH - 1 -: WIDTH];
 end
 wire [WIDTH - 1:0] ram_q_a, ram_q_b;
-dual_port_block_ram(clk, wr[{1'H0, counter_pipe}], addr_2d[{1'H0, counter_pipe}], d_2d[{1'H0, counter_pipe}], ram_q_a, wr[{1'H1, counter_pipe}], addr_2d[{1'H1, counter_pipe}], d_2d[{1'H1, counter_pipe}], ram_q_b);
+dual_port_block_ram ram(clk, wr[{1'H0, counter}], addr_2d[{1'H0, counter}], d_2d[{1'H0, counter}], ram_q_a, wr[{1'H1, counter}], addr_2d[{1'H1, counter}], d_2d[{1'H1, counter}], ram_q_b);
 
-//TODO
+always @(posedge clk) begin
+    for(i = 1; i < PORTS / 2; i = i + 1) begin
+        if((i + 1) % (PORTS / 2) == counter) begin
+            q[(i + 1) * WIDTH - 1 -: WIDTH] <= ram_q_a;
+            q[(i + PORTS / 2 + 1) * WIDTH - 1 -: WIDTH] <= ram_q_b;
+        end
+    end
+end
+`include "common.vh"
 endmodule
