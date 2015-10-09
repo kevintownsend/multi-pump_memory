de0293eee403358af769f7fe4ac9203dee17be5e
Kevin Townsend <kt@krtownsend.com>
2015-10-08 23:08:15 -0500

    .

--- compile.tcl
+++ compile.tcl
@@ -10,8 +10,8 @@ create_clock -period 2.50 clk
 write_checkpoint -force $outputDir/post_synth.dcp
 report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
 report_utilization -file $outputDir/post_synth_util.rpt
-opt_design
-place_design
-route_design
-report_timing_summary -file $outputDir/post_route_timing_summary.rpt
-report_utilization -file $outputDir/post_route_util.rpt
+#opt_design
+#place_design
+#route_design
+#report_timing_summary -file $outputDir/post_route_timing_summary.rpt
+#report_utilization -file $outputDir/post_route_util.rpt
